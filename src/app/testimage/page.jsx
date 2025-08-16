20b917e29eaaf44da3f6da6b4d58fe1abd999bdc
milad998 <myaralbwd20@gmail.com>
2025-08-16 09:02:58 +0300

    Update page.jsx

--- src/app/testimage/page.jsx
+++ src/app/testimage/page.jsx
@@ -2,7 +2,7 @@ export default function TestImg() {
   return (
     <div >
       <main >
-        <img src="./benefit.jpg" alt="benefit logo" />
+        <img src="./file_0000000093a061f499e55911fc6b9a2e.png" alt="benefit logo" />
       </main>
     </div>
   );
